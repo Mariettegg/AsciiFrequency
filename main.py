bafecb588a6151998d53ac3a45d44c32f882e753
Mariettegg <100428980@alumnos.uc3m.es>
2022-02-17 00:25:46 -0500

    Program finished, but errors regarding inputs are not solved.

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+#Written by Mario Gomez
+#mariogomezmartin11@gmail.com
+
+#### OBVSERVATIONS ####
+# 1. This time did not get enough time to manage the interrupts
+#    possible errors that may happen when introducing the wrong
+#    commands in the console.
+#
+# 2. Also may want to install first the libraries listed below as
+#    i got some problems trying to get them installed in my pycharm.
+#
+# 3. Please have the files of the texts you are going to analyse in
+#    the same folder as this file, if not it wont work.
+
 from matplotlib import pyplot as plt
 import numpy as np
+
 
 action = input("Action: (1) Input text, (2) Input file >> ")
 option = input("Type of search: (1) All ascii values or (2) Alpha-numerical values only >> ")
@@ -18,12 +33,20 @@ else:
     text.append(textInput)
 
 recordAlphaNum = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0')
+
+# We have two different lists because if we want to see if they appear in the text, we need the actual value, but if we try to put that actual value in the axis, the value "written" wont appear
 asciiList = (['\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07', '\x08', '\t', '\n', '\x0b', '\x0c', '\r', '\x0e', '\x0f', '\x10', '\x11', '\x12',
           '\x13', '\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1a', '\x1b', '\x1c', '\x1d', '\x1e', '\x1f',
           ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
           ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
           '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '\x7f'])
+asciiListPlot = (['\\x00', '\\x01', '\\x02', '\\x03', '\\x04', '\\x05', '\\x06', '\\x07', '\\x08', '\\t', '\\n', '\\x0b', '\\x0c', '\\r', '\\x0e', '\\x0f', '\\x10', '\\x11', '\\x12',
+          '\\x13', '\\x14', '\\x15', '\\x16', '\\x17', '\\x18', '\\x19', '\\x1a', '\\x1b', '\\x1c', '\\x1d', '\\x1e', '\\x1f',
+          ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+          ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+          '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '\\x7f'])
 record = {}
+freqTable = {}
 totalCharacters = 0
 
 if int(option) == 1:
@@ -44,23 +67,32 @@ if int(option) == 1:
         else:
             dev_x.append(0)
 
-    dev_y = np.arange(len(asciiList))
-    plt.xticks(dev_y, asciiList)
+    counter = 0
+    for x in asciiListPlot:
+        freqTable[x] = dev_x[counter]
+        counter += 1
+    print(freqTable)
+
+    dev_y = np.arange(len(asciiListPlot))
+    plt.xticks(dev_y, asciiListPlot)
 
 
 
 else:
     for line in text:
         for character in line:
-            totalCharacters += 1
+
             if character in record.keys():
                 record[character] += 1
+                totalCharacters += 1
 
             elif character.lower() in record.keys():
                 record[character.lower()] += 1
+                totalCharacters += 1
 
             elif character.lower() in recordAlphaNum:
                 record[character.lower()] = 1
+                totalCharacters += 1
 
     dev_y = recordAlphaNum
     dev_x = []
@@ -72,6 +104,12 @@ else:
 
     dev_y = np.arange(len(recordAlphaNum))
     plt.xticks(dev_y, recordAlphaNum)
+
+    counter = 0
+    for x in recordAlphaNum:
+        freqTable[x] = dev_x[counter]
+        counter += 1
+    print(freqTable)
 
 
 plt.bar(dev_y,dev_x)
